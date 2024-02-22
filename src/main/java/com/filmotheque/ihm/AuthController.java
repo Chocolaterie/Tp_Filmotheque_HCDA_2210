c141c83f35f04f939c9d044899c7cb0053ecba5b
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-02-22 10:02:31 +0100

    Login form

--- src/main/java/com/filmotheque/ihm/AuthController.java
+++ src/main/java/com/filmotheque/ihm/AuthController.java
@@ -3,24 +3,36 @@ package com.filmotheque.ihm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import com.filmotheque.bo.Member;
 
 @Controller
 @SessionAttributes({"loggedUser"})
 public class AuthController {
 
 	@GetMapping("login")
-	public String loginPage() {
+	public String loginPage(Model model) {
+		
+		// Envoyer un membre vide par défaut dans le formulaire login
+		// pour le th:object
+		model.addAttribute("member", new Member());
+		
 		return "login-page";
 	}
 	
-	@GetMapping("login-test")
-	public String loginTest(Model model) {
+	@PostMapping("login")
+	public String loginTest(@ModelAttribute("member") Member member, Model model) {
+		// Controle de surface (ex: regex sur email)
+		// Controle métier (si en base de donnée) (le service, donc le manager)
 		
-		model.addAttribute("loggedUser", "Tata@gmail.com");
+		// Envoyer une donnée en session
+		model.addAttribute("loggedUser", member);
 		
-		return "redirect:/";
+		return "redirect:/movies";
 	}
 	
 	@GetMapping("logout")
