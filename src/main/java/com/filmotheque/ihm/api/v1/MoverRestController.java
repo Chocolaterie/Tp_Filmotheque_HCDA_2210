79f03d4b31b42d745a4a8f7b25afb5d53eb7fb9c
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-02-28 09:56:26 +0100

    Update MoverRestController.java

--- src/main/java/com/filmotheque/ihm/api/v1/MoverRestController.java
+++ src/main/java/com/filmotheque/ihm/api/v1/MoverRestController.java
@@ -3,6 +3,7 @@ package com.filmotheque.ihm.api.v1;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.filmotheque.bo.Movie;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin
 public class MoverRestController {
 
 	@Autowired
